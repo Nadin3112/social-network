6ed9c7327e7fc8ea15e35fa402be7462649033fe
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-02-11 13:01:44 +0300

    adds header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import S from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={S.header}>
+            <img src='https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1788068356.1707177600&semt=ais' alt='' />
+        </header>
+    );
+};
+
+export default Header;
