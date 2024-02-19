35b686f7949fe9a1b4f2963336e5a584e041f33e
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-02-19 10:30:39 +0300

    adds dialogs

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -2,31 +2,38 @@ import React from 'react';
 import S from './Dialogs.module.css';
 import DialogItem from './dialogItem/DialogItem';
 import MessageItem from './message/MessageItem';
+import { sendMessageActionCreator, updateNewMessageTextActionCreator } from '../../redux/state';
 
 const Dialogs = (props) => {
-    
-    let dialogsElements = props.state.dialogs.map(d => <DialogItem id={d.id} name={d.name} />);
-    let messagesElements =  props.state.messages.map(m => <MessageItem message={m.message} id={m.id} />)
-    
-    let newMessageElement = React.createRef();
 
-    let appMessage = () => {
-        let message = newMessageElement.current.value;
-        alert(message);
+    let dialogsElements = props.dialogsPage.dialogs.map(d => <DialogItem id={d.id} name={d.name} />);
+    let messagesElements = props.dialogsPage.messages.map(m => <MessageItem message={m.message} id={m.id} />)
+
+    let newMessageElement = props.dialogsPage.newMessageText;
+
+    let onSendMessageClick = () => {
+        props.dispatch(sendMessageActionCreator())
+    }
+
+    let onNewMessageChange = (e) => {
+        let message = e.target.value;
+        props.dispatch(updateNewMessageTextActionCreator(message));
     }
 
     return (
         <div className={S.dialogs}>
-            <ul className={S.dialogList}> 
+            <ul className={S.dialogList}>
                 {dialogsElements}
             </ul>
-            <ul className={S.messageList}> 
-                {messagesElements}
-            </ul>
-            <form className={S.form}>
-                <textarea ref={newMessageElement}></textarea>
-                <button type='button' className={S.btn} onClick={appMessage}>Add message</button>
-            </form>
+            <div>
+                <ul className={S.messageList}>
+                    {messagesElements}
+                </ul>
+                <form className={S.form}>
+                    <textarea value={newMessageElement} onChange={onNewMessageChange} placeholder='Enter your message'/> 
+                    <button type='button' className={S.btn} onClick={onSendMessageClick}>Send message</button>
+                </form>
+            </div>
         </div>
     );
 };
