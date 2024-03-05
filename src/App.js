af217d84f55a266d7aafb7ee0ff5fcf845245b2d
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-03-05 10:49:31 +0300

    refractoring profile

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import NavBar from './components/navBar/NavBar';
-import Profile from './components/profile/Profile';
 import News from './components/news/News';
 import Music from './components/music/Music';
 import Settings from './components/settings/Settings';
 import { Route, Routes } from 'react-router-dom';
 import DialogsContainer from './components/dialogs/DialogsContainer';
 import UsersContainer from './components/users/UsersContainer';
+import ProfileContainer from './components/profile/ProfileContainer';
 
 
 const App = (props) => {
@@ -19,7 +19,7 @@ const App = (props) => {
       <NavBar />
       <div className='app-wrapper-content'>
         <Routes>
-          <Route path='/profile/*' element={<Profile />} />
+          <Route path='/profile/*' element={<ProfileContainer />} />
           <Route path='/dialogs/*' element={<DialogsContainer />} />
           <Route path='/news' element={<News />} />
           <Route path='/music' element={<Music />} />
--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import S from './ProfileInfo.module.css';
+import Preloader from '../../common/preloader/Preloader';
 
-const ProfileInfo = () => {
+const ProfileInfo = (props) => {
+    if(!props.profile) {
+        return <Preloader/>
+    }
     return (
             <div className={S.profileImg}>
                 <img src='https://kartinki.pics/uploads/posts/2022-05/thumbs/1652228067_2-kartinkin-net-p-krasivie-kartinki-dusha-2.jpg' alt='' />
+            <div>
+                <img src={props.profile.photos.large} alt=""></img>
+            </div>
             </div>
     );
 };
