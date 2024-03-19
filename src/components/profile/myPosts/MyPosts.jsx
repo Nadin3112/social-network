f6a98b9dd555eff50dbbb063112566b3689ede96
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-03-19 12:32:58 +0300

    adds memo

--- src/components/profile/myPosts/MyPosts.jsx
+++ src/components/profile/myPosts/MyPosts.jsx
@@ -8,30 +8,39 @@ import { Textarea } from '../../common/formsControls/FormControls';
 const maxLengthCreator10 = maxLengthCreator(10);
 
 
-const MyPosts = (props) => {
-    const onAddPost = (values) => {props.addPost(values.newPostElement)};
+const MyPosts = React.memo(props => {
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextProps !== this.props || nextState !== this.state;
+    // }
+
+    const onAddPost = (values) => {
+        props.addPost(values.newPostElement)
+    };
+
     let postsElements = props.posts.map(p => <Post message={p.message} likeCount={p.likeCount} key={p.id} id={p.id} />);
     return (
         <div className={S.myPosts}>
             <h3>My posts</h3>
-            <AddNewPostReduxForm  onSubmit={onAddPost} />
+            <AddNewPostReduxForm onSubmit={onAddPost} />
             <div className={S.posts}>{postsElements}</div>
         </div>
-    );
-};
+    )
+});
+
 
 const AddNewPostForm = (props) => {
     return (
         <form className={S.form} onSubmit={props.handleSubmit}>
-            <Field 
-            component={Textarea} 
-            name="newPostElement" 
-            validate={[required, maxLengthCreator10]}/>
+            <Field
+                component={Textarea}
+                name="newPostElement"
+                validate={[required, maxLengthCreator10]} />
             <button className={S.btn}>Add post</button>
         </form>
     );
 };
 
-const AddNewPostReduxForm = reduxForm({form: "ProfileAddNewPostForm"})(AddNewPostForm)
+const AddNewPostReduxForm = reduxForm({ form: "ProfileAddNewPostForm" })(AddNewPostForm)
 
 export default MyPosts;
