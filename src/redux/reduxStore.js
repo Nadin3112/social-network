571198406b3c836a3e42b7318a5ef553e0c99a37
Nadia Padalitsa <lady.mancko2009@yandex.ru>
2024-03-20 10:31:34 +0300

    adds REDUX_DEVTOOLS_EXTENSION

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import {  applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
+import { compose, applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
 import dialogsReducer from "./dialogsReducer";
 import profileReducer from "./profileReducer";
 import sidebarReducer from "./sidebarReducer";
@@ -20,8 +20,9 @@ let reducers = combineReducers({
     app: appReducer
 });
 
-let  store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store;
+window.__store__ = store;
 
 export default store;
